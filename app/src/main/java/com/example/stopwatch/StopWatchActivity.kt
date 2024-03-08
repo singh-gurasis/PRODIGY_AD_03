95926ed94d8b60f6cff22379b38af0ff916ba53b
Gurasis Singh <gurasis056@gmail.com>
2024-03-08 20:23:54 +0530

    finished project

--- app/src/main/java/com/example/stopwatch/StopWatchActivity.kt
+++ app/src/main/java/com/example/stopwatch/StopWatchActivity.kt
@@ -1,12 +1,17 @@
 package com.example.stopwatch
 
+import android.graphics.drawable.AnimationDrawable
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
+import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import com.example.stopwatch.databinding.ActivityStopWatchBinding
+import java.util.Date
 
 class StopWatchActivity : AppCompatActivity() {
 
@@ -24,7 +29,12 @@ class StopWatchActivity : AppCompatActivity() {
         binding.btnstop.setAlpha(0F)
 
         binding.btnstart.setOnClickListener {
+
             onClick()
+
+        }
+        binding.btnstop.setOnClickListener {
+            onPauseClick()
         }
     }
 
@@ -36,4 +46,19 @@ class StopWatchActivity : AppCompatActivity() {
         binding.timer.setBase(SystemClock.elapsedRealtime())
         binding.timer.start()
     }
+
+    private fun onPauseClick(){
+
+        binding.icanchor.clearAnimation()
+
+        binding.btnstop.animate().alpha(0F).setDuration(300).start()
+        binding.btnstart.animate().alpha(1F).setDuration(300).start()
+        binding.timer.stop()
+
+        val res = "Restart"
+        binding.btnstart.text = res
+
+    }
+
 }
+
